7a27180e26bc82bbac2f65e312e49879f077a196
Karol Piasecki <karolpiasecki21@gmail.com>
2019-01-11 14:08:07 +0100

    add eq ne built in func and validator for chain

--- blockchain.py
+++ blockchain.py
@@ -19,9 +19,23 @@ class BlockChain:
         return json.dumps(self, default=lambda o: o.__dict__,
            sort_keys=True, indent=4)
 
-    def chain_validator():
+    def chain_validator(self):
         gen_block = self.genesis_block()
-        if(gen_block != genesis_block()):
+
+        if(gen_block != self.chain[0]):
+            return False
+
+        for i in range(1, len(self.chain)-1):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+
+            if((current_block.hash != current_block.calculate_hash())):
+                return False
+
+            if((current_block.prev_hash != previous_block.calculate_hash())):
+                return False
+
+        return True
 
 
 class Transaction:
@@ -29,10 +43,25 @@ class Transaction:
         self.f_address = fAddress
         self.t_address = tAddress
         self.amount = amount
+        self.timestamp="2/11/2019" #date.now()
 
     def __str__(self):
         return json.dumps(self, default=lambda o: o.__dict__,
            sort_keys=True, indent=4)
+
+    def __eq__(self, other):
+        return (self.timestamp == other.timestamp
+                    and self.fAddress == other.fAddress
+                    and self.tAddress == other.tAddress
+                    and self.amount == other.amount)
+
+    def __ne__(self, other):
+        return (self.timestamp != other.timestamp
+                    and self.fAddress != other.fAddress
+                    and self.tAddress != other.tAddress
+                    and self.amount != other.amount)
+
+
 
 class Block:
     def __init__(self, timestamp, transactions, prevHash = ""):
@@ -45,6 +74,16 @@ class Block:
         return json.dumps(self, default=lambda o: o.__dict__,
            sort_keys=True, indent=2)
 
+    def __eq__(self, other):
+        return (self.timestamp == other.timestamp
+                    and self.transtactions == other.transactions
+                    and self.hash == other.prev_has)
+
+    def __ne__(self, other):
+        return (self.timestamp != other.timestamp
+                    and self.transtactions != other.transactions
+                    and self.hash != other.prev_has)
+
     def encode_v(self, value):
         return str(value).encode('utf-8')
 
@@ -55,8 +94,8 @@ class Block:
         return self.hash
 
     def calculate_hash(self):
-        return hashlib.sha256(self.encode_v(self.timestamp)+
-                     self.encode_v(self.transactions)
+        return hashlib.sha256(self.encode_v(self.timestamp)
+                     +self.encode_v(self.transactions)
                      + self.encode_v(self.prev_hash)).hexdigest()
 
 
@@ -64,3 +103,9 @@ b = BlockChain()
 b.mine_block()
 b.mine_block()
 print (str(b))
+print(b.chain_validator())
+print("-"*10+"fail chain"+"-"*10)
+b.chain[1].hash = "1234123"
+print(str(b))
+print(b.chain_validator())
+
